1498779bea816aa21b0393c9029a0bc49e331a97
darasi <zuradaraselia2@gmail.com>
2017-12-26 19:30:37 +0400

    commit10

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,27 +2,9 @@ import React, { Component } from 'react';
 import { Container, Menu } from 'semantic-ui-react';
 import { Link as ScrollLink } from 'react-scroll';
 
-const Header = (props) =>
+class Header extends Component {
 
-  <header className="header" id="header">
-    <Container>
-      <Menu secondary stackable floated="right" className="header-menu">
-        {props.headerMenu.map((item, index) => (
-          <Menu.Item key={index} name={item.title}>
-            <ScrollLink 
-              activeClass="active"
-              to={item.section}
-              spy={true} 
-              smooth={true} 
-              hashSpy={true} 
-              duration={500}>{item.title}
-            </ScrollLink>
-          </Menu.Item>
-        ))}
-      </Menu>
-    </Container>
-  </header>
-  {
+  componentDidMount() {
     window.onscroll = function() {myFunction()};
     function myFunction() {
         if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
@@ -32,5 +14,35 @@ const Header = (props) =>
         }
     }
   }
+
+  constructor(props) {
+    super(props);
+    // this.state = {
+    //   color: props.initialColor
+    // };
+  }
+
+  render() {
+    return <header className="header" id="header">
+            <Container>
+              <Menu secondary stackable floated="right" className="header-menu">
+                {this.props.headerMenu.map((item, index) => (
+                  <Menu.Item key={index} name={item.title}>
+                    <ScrollLink 
+                      activeClass="active"
+                      to={item.section}
+                      spy={true} 
+                      smooth={true} 
+                      hashSpy={true} 
+                      duration={500}>{item.title}
+                    </ScrollLink>
+                  </Menu.Item>
+                ))}
+              </Menu>
+            </Container>
+          </header>
+  }
+
+}
 
 export default Header;
